bd290dbe208796434f03825f68d58c3189c103ab
M V Kiran <mada.kiran@s.amity.edu>
2026-05-10 17:38:28 +0530

    Create bisection_method.py

---
+++ 01_Root_Finding/bisection_method.py
@@ -0,0 +1,31 @@
+import math
+
+# Function defined using user input
+def f(x):
+    return eval(func)
+
+def bisection(a, b, tol):
+    if f(a) * f(b) >= 0:
+        print("Bisection method fails. f(a) and f(b) must have opposite signs.")
+        return
+
+    print("Iter\t a\t\t b\t\t c\t\t f(c)")
+
+    iteration = 1
+    c = 0
+
+    while (b - a) / 2 > tol:
+        c = (a + b) / 2
+
+        print(iteration, "\t", round(a,6), "\t", round(b,6), "\t", round(c,6), "\t", round(f(c),6))
+
+        if f(c) == 0:
+            break
+        elif f(a) * f(c) < 0:
+            b = c
+        else:
+            a = c
+
+        iteration += 1
+
+    print("\nApproximate root =", round(c,6))
