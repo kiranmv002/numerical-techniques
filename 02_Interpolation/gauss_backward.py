146f4da2b7d1b92e9f3783f9b0c74856c5a9970f
M V Kiran <mada.kiran@s.amity.edu>
2026-05-24 20:14:41 +0530

    Create gauss_backward.py

---
+++ 02_Interpolation/gauss_backward.py
@@ -0,0 +1,47 @@
+# Gauss Backward Difference with user input
+
+# Input
+n = int(input("Enter number of data points: "))
+x = list(map(float, input("Enter x values: ").split()))
+y = list(map(float, input("Enter y values: ").split()))
+
+# Build forward difference table (same table used)
+diff = [y.copy()]
+
+for i in range(1, n):
+    temp = []
+    for j in range(n - i):
+        temp.append(diff[i-1][j+1] - diff[i-1][j])
+    diff.append(temp)
+
+# Display table
+print("\nForward Difference Table:")
+for i in range(n):
+    print(x[i], end="\t")
+    for j in range(n - i):
+        print(diff[j][i], end="\t")
+    print()
+
+# Interpolation
+xp = float(input("\nEnter value to interpolate: "))
+h = x[1] - x[0]
+
+# Central index (take middle)
+m = n // 2
+
+u = (xp - x[m]) / h
+
+result = y[m]
+term = 1
+
+for i in range(1, n):
+    if i % 2 != 0:
+        k = (i + 1) // 2
+        term = term * (u + (k - 1)) / i
+        result += term * diff[i][m - k]
+    else:
+        k = i // 2
+        term = term * (u - (k - 1)) / i
+        result += term * diff[i][m - k]
+
+print("\nInterpolated value =", result)
