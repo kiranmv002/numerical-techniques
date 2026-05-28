974cf338b84ab33da6704bfbdfc278b5516c0e23
M V Kiran <mada.kiran@s.amity.edu>
2026-05-28 20:42:19 +0530

    Update sterlings_interpolation.py

--- 02_Interpolation/sterlings_interpolation.py
+++ 02_Interpolation/sterlings_interpolation.py
@@ -22,3 +22,26 @@ for i in range(n):
         print(diff[j][i], end="\t")
     print()
 
+# Interpolation
+xp = float(input("\nEnter value to interpolate: "))
+h = x[1] - x[0]
+
+# Central index
+m = n // 2
+u = (xp - x[m]) / h
+result = y[m]
+
+# Stirling formula terms
+term1 = u * (diff[1][m-1] + diff[1][m]) / 2
+result += term1
+
+term2 = (u*u / 2) * diff[2][m-1]
+result += term2
+
+term3 = (u*(u*u - 1) / 6) * (diff[3][m-2] + diff[3][m-1]) / 2
+result += term3
+
+term4 = (u*u*(u*u - 1) / 24) * diff[4][m-2]
+result += term4
+
+print("\nInterpolated value =", result)
