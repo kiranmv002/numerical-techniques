3e9c4847311e2e91e5877d984aaf564c78bb141c
M V Kiran <mada.kiran@s.amity.edu>
2026-07-05 17:57:53 +0530

    Create runge_kutta_4.py

---
+++ 06_ODE_Solvers/runge_kutta_4.py
@@ -0,0 +1,11 @@
+# RK4 Method with User Input Function
+# Input the differential equation
+func = input("enter dy/dx=")
+def f(x,y): 
+    return eval(func) 
+#input initial values 
+x = float(input("Enter initial value of x0: "))
+y = float(input("Enter initial value of y0: ")) 
+n = int(input("Enter number of steps: "))
+h = float(input("Enter step size h: "))
+print("\nStep\t x\t\t y")
