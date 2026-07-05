efdf17f092f2dd5ee3345f15cb65dc5ab170b1de
M V Kiran <mada.kiran@s.amity.edu>
2026-07-05 17:59:53 +0530

    Update modified_euler.py

--- 06_ODE_Solvers/modified_euler.py
+++ 06_ODE_Solvers/modified_euler.py
@@ -9,3 +9,11 @@ y = float(input("Enter initial value of y0: "))
 n = int(input("Enter number of steps: "))
 h = float(input("Enter step size h: "))
 print("\nStep\t x\t\t y")
+print(f"0\t {x:.4f}\t {y:.4f}")
+for i in range(1, n + 1):
+    # Predictor step
+    y_predict = y + h * f(x, y)
+    # Corrector step
+    y = y + (h / 2) * (f(x, y) + f(x + h, y_predict))
+    x = x + h
+    print(f"{i}\t {x:.4f}\t {y:.4f}")
