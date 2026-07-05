76d2b05d6736a59850f8c6b72d0abf3026aa76a6
M V Kiran <mada.kiran@s.amity.edu>
2026-07-05 18:02:01 +0530

    Update runge_kutta_4.py

--- 06_ODE_Solvers/runge_kutta_4.py
+++ 06_ODE_Solvers/runge_kutta_4.py
@@ -9,3 +9,12 @@ y = float(input("Enter initial value of y0: "))
 n = int(input("Enter number of steps: "))
 h = float(input("Enter step size h: "))
 print("\nStep\t x\t\t y")
+print(f"0\t {x:.4f}\t {y:.4f}")     
+for i in range(1, n + 1):
+    k1 = h * f(x, y)
+    k2 = h * f(x + h, y + k1)
+    k3 = h * f(x + h / 2, y + k1 / 2)
+    k4 =h * f(x + h / 2, y + k2 / 2)
+    y = y + (k1 + 2 * k2 + 2 * k3 + k4) / 6
+    x = x + h
+    print(f"{i}\t {x:.4f}\t {y:.4f}")
