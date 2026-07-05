ad6d17115f5c6140ef5f6e4e4ae930ea80c20a65
M V Kiran <mada.kiran@s.amity.edu>
2026-07-05 17:56:20 +0530

    Create eulers_method.py

---
+++ 06_ODE_Solvers/eulers_method.py
@@ -0,0 +1,16 @@
+# Euler's Method with User Input Function
+# Input the differential equation
+func = input("enter dy/dx=")
+def f(x,y): 
+    return eval(func) 
+#input initial values 
+x = float(input("Enter initial value of x0: "))
+y = float(input("Enter initial value of y0: ")) 
+n = int(input("Enter number of steps: "))
+h = float(input("Enter step size h: "))
+print("\nStep\t x\t\t y")
+print(f"0\t {x:.4f}\t {y:.4f}")
+for i in range(1, n + 1):
+    y = y + h * f (x,y)
+    x = x + h
+    print(f"{i}\t {x:.4f}\t {y:.4f}")
