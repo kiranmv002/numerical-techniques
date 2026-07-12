396d56126c965c202cfbae03d4b0ea77a343d7b8
M V Kiran <mada.kiran@s.amity.edu>
2026-07-12 20:30:12 +0530

    Update fitted_exponential_curve.py

--- 07_Curve_Fitting/fitted_exponential_curve.py
+++ 07_Curve_Fitting/fitted_exponential_curve.py
@@ -26,3 +26,18 @@ B = (n * sumXY - sumX * sumY) / (n * sumX2 - sumX**2)
 # Find A = ln(a)
 A = (sumY - B * sumX) / n
 
+# Convert 
+a = math.exp(A)
+b = math.exp(B)
+
+print("\na =", round(a, 4))
+print("b =", round(b, 4))
+
+print("\nFitted Exponential Curve:")
+print("y =", round(a, 4), "* x^", round(b, 4))
+
+# Estimate y
+xp = float(input("\nEnter x value to estimate y: "))
+y_est = a * (b ** xp)
+
+print("Estimated y =", round(y_est, 4))
