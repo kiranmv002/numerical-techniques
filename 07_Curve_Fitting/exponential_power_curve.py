78c9f93160f64620934a6c099f1622c0aa57592d
M V Kiran <mada.kiran@s.amity.edu>
2026-07-12 20:26:56 +0530

    Create exponential_power_curve.py

---
+++ 07_Curve_Fitting/exponential_power_curve.py
@@ -0,0 +1,27 @@
+import math
+
+# Input
+n = int(input("Enter the number of data points: "))
+x = []
+y = []
+
+for i in range(n):
+    x.append(float(input(f"Enter x: ")))
+    y.append(float(input(f"Enter y: ")))
+
+# Transform y to ln(y)
+X = x
+Y = [math.log(val) for val in y]
+
+# Calculate sums
+sumX = sum(X)
+sumY = sum(Y)
+sumXY = sum(X[i] * Y[i] for i in range(n))
+sumX2 = sum(X[i]**2 for i in range(n))
+
+# Find b
+b = (n * sumXY - sumX * sumY) / (n * sumX2 - sumX**2)
+
+# Find a
+A = (sumY - b * sumX) / n
+a = math.exp(A)
