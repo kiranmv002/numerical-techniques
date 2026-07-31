d9b3ce56e362c73f6a8e995ec4d9d8f40ded1ca1
M V Kiran <mada.kiran@s.amity.edu>
2026-07-31 23:22:20 +0530

    Create  polynomial_diff_equal_intervals.py

---
+++ 05_Polynomial_Differentiation/polynomial_diff_equal_intervals.py
@@ -0,0 +1,26 @@
+#finding polynomial, differentiation and estimation
+# for equal interval data
+from sympy import symbols, diff, expand
+n = int(input("Enter number of data points: "))
+x = []
+y = []
+for i in range(n):
+    x.append(float(input(f"Enter x[{i}]: ")))
+    y.append(float(input(f"Enter y[{i}]: ")))
+h = x[1] - x[0] # equal interval
+#difference table
+diff_table = [y.copy()]
+for i in range(1, n):
+    temp = []
+    for j in range(n - i):
+        temp.append(diff_table[i-1][j+1] - diff_table[i-1][j])
+    diff_table.append(temp) # display forward difference table
+print("\nForward Difference Table:")
+print("-" * 60)
+print("x\t y\t Δy\t Δ²y\t Δ³y \t Δ⁴y")
+for i in range(n):
+    print(x[i], end="")
+    for j in range(n - i):
+        print(round(diff_table[j][i], 4), end="\t")
+    print()
+X = symbols('X')
